66383af03f9fc1607b4d0243641dd4d3c21618f6
maggiehubbard <maggiehubbard@wayne.edu>
2018-06-07 09:13:32 -0400

    first commit

---
+++ DeckofCards/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeckofCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Make a call to the api to generate a new deck
+            HttpWebRequest WR = WebRequest.CreateHttp("https://deckofcardsapi.com/api/deck/new/draw/?count=5");
+            WR.UserAgent = ".NET Framework Test Client";
+
+            HttpWebResponse Response;
+
+            try
+            {
+                Response = (HttpWebResponse)WR.GetResponse();
+            }
+            catch (WebException e)
+            {
+                ViewBag.Error = "Exception";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+            if (Response.StatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.Error = Response.StatusCode;
+                ViewBag.ErrorDescription = Response.StatusDescription;
+                return View();
+            }
+
+            StreamReader reader = new StreamReader(Response.GetResponseStream());
+            string DeckOfCardsAPI = reader.ReadToEnd();
+
+            try
+            {
+                JObject JsonData = JObject.Parse(DeckOfCardsAPI);
+                
+                    ViewBag.DeckID = JsonData["deck_id"];
+                    ViewBag.Remaining = JsonData["remaining"];
+                ViewBag.Cards = (JArray)JsonData.SelectToken("cards");
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = "JSON Issue";
+                ViewBag.ErrorDescription = e.Message;
+                return View();
+            }
+
+            return View();
+            
+        }
+
+    }
+}
